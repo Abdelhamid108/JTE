1b3ba23536098b52164fa7871957f37994767ae9
Abdelhamid108 <abdelhamidmido10@gmail.com>
2026-08-07 22:11:29 +0300

    FIX: Update terraform.init step to do it apply it with credintails to solve the remote s3 state problem

--- libraries/terraform/steps/init.groovy
+++ libraries/terraform/steps/init.groovy
@@ -2,9 +2,20 @@
 
 void call() {  
     String targetDir = config.infra_dir ?: '.'
+    String cloudCreds = config.cloud_creds ?: 'NONE'
     
-    echo "Initializing Terraform..."
-    dir(targetDir) {
-        sh "terraform init -reconfigure"
+    def runInit = {
+            echo "Initializing Terraform..."
+            dir(targetDir) {
+                sh "terraform init -reconfigure"
+            }
+    }
+
+    if (cloudCreds && cloudCreds != 'NONE') {
+        withCredentials([aws(credentialsId: cloudCreds, accessKeyVariable: 'AWS_ACCESS_KEY_ID', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
+            runInit()
+        }
+    } else {
+        runInit()
     }
 }
