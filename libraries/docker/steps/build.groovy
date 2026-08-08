9c0a95a40eec0d3b58535c7e9dffb2a3b8cff9e5
Abdelhamid108 <abdelhamidmido10@gmail.com>
2026-08-08 04:27:19 +0300

    refactor: namespace library steps by prefixing with terraform, docker, and npm, and fix syntax errors in docker build step

--- libraries/docker/steps/build.groovy
+++ libraries/docker/steps/build.groovy
@@ -1,19 +1,19 @@
 // steps/build.groovy
 
-String call (Map args = [:]) {
+String call(Map args = [:]) {
     String imageName = args.image_name ?: config.image_name ?: 'NONE' 
     String dockerFileName = args.docker_file_name ?: config.docker_file_name ?: 'Dockerfile'
     String buildDir = args.dockerfile_dir ?: config.docker_file_dir ?: '.'
     boolean noCache = config.no_cache ?: false
     String noCacheFlag = noCache ? "--no-cache" : ""
 
-    if (imageName != 'NONEs'){    
+    if (imageName != 'NONE') {    
         String pipelineImage = "${imageName}:build-${env.BUILD_ID}"
 
         echo "Building Docker Image: ${pipelineImage}"
 
-        dir(buildDir){
-            docker build -f ${dockerFileName} -t ${pipelineImage} ${noCacheFlag} .
+        dir(buildDir) {
+            sh "docker build -f ${dockerFileName} -t ${pipelineImage} ${noCacheFlag} ."
             return pipelineImage
         }
     } else {
