cfb7c1a14459a627128bd63f9bf503317e6000b7
Abdelhamid108 <abdelhamidmido10@gmail.com>
2026-08-09 00:50:47 +0300

    feat: allow configurable manifests checkout directory in checkOutRemoteSCM step

--- libraries/kubernetes/steps/checkOutRemoteSCM.groovy
+++ libraries/kubernetes/steps/checkOutRemoteSCM.groovy
@@ -1,22 +1,23 @@
 // kubernetes/steps/checkOutRemoteSCM.groovy
 
 void call(Map args = [:]) {
-    String gitCreds = args.git_creds ?: config.manifests_git_creds
-    String repoUrl  = args.repo_url  ?: config.manifests_repo_url
-    String branch   = args.branch    ?: config.manifests_branch ?: 'main'
+    String gitCreds  = args.git_creds     ?: config.manifests_git_creds
+    String repoUrl   = args.repo_url      ?: config.manifests_repo_url
+    String branch    = args.branch        ?: config.manifests_branch ?: 'main'
+    String targetDir = args.manifests_dir ?: config.manifests_dir     ?: 'manifests-repo'
 
     if (!gitCreds || !repoUrl) {
         error "checkOutRemoteSCM: 'git_creds' and 'repo_url' are required to checkout the manifests repository."
     }
 
-    echo "Checking out manifests repo: ${repoUrl} (branch: ${branch})"
+    echo "Checking out manifests repo: ${repoUrl} (branch: ${branch}) to '${targetDir}'"
 
     checkout([
         $class: 'GitSCM',
         branches: [[name: "*/${branch}"]],
         userRemoteConfigs: [[credentialsId: gitCreds, url: repoUrl]],
-        extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'manifests-repo']]
+        extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: targetDir]]
     ])
 
-    echo "Manifests repository checked out to 'manifests-repo/'"
+    echo "Manifests repository checked out to '${targetDir}/'"
 }
