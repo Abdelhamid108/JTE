5a8380aee9dfc2332017a6e529d579e81a727dac
Abdelhamid108 <abdelhamidmido10@gmail.com>
2026-08-09 22:19:10 +0300

    refactor: simplify deploy logic by removing redundant flag handling and streamlining playbook execution with credentials

--- libraries/ansible/steps/deploy.groovy
+++ libraries/ansible/steps/deploy.groovy
@@ -2,41 +2,25 @@
 
 void call(Map args = [:]) {
     String targetDir  = args.playbook_dir ?: config.playbook_dir ?: '.'
-    String playbook    = args.playbook_file ?: config.playbook_file ?: 'site.yml'
-    String inventory   = args.inventory_file ?: config.inventory_file ?: 'inventory.ini'
-    String sshCreds    = args.ssh_creds ?: config.ssh_creds ?: 'NONE'
-    
-    String rawExtraVars = args.extra_vars ?: config.extra_vars
-    String extraVars   = rawExtraVars ? "--extra-vars \"${rawExtraVars}\"" : ""
+    String playbook   = args.playbook_file ?: config.playbook_file ?: 'site.yml'
+    String inventory  = args.inventory_file ?: config.inventory_file ?: 'inventory.ini'
+    String sshCreds   = args.ssh_creds ?: config.ssh_creds ?: 'NONE'
 
-    boolean becomeFlagSet = args.become != null ? args.become.toString().toBoolean() : (config.become != null ? config.become.toString().toBoolean() : false)
-    String becomeFlag  = becomeFlagSet ? "--become" : ""
+    dir(targetDir) {
+        if (!fileExists(inventory)) {
+            error "PIPELINE STOPPED: Inventory file '${inventory}' not found in ${targetDir}. Run fetchInventory() first."
+        }
 
-    def runPlaybook = {
-        dir(targetDir) {
-            if (!fileExists(inventory)) {
-                error "PIPELINE STOPPED: Inventory file '${inventory}' not found in ${targetDir}. Run fetchInventory() first."
-            }
-            if (env.ANSIBLE_SSH_KEY) {
-                try {
-                    sh "cp \"${env.ANSIBLE_SSH_KEY}\" ./ansiblekey.pem && chmod 400 ./ansiblekey.pem"
-                    sh "ansible-playbook -i ${inventory} ${playbook} ${becomeFlag} ${extraVars}"
-                } finally {
-                    sh "rm -f ./ansiblekey.pem"
+        if (sshCreds && sshCreds != 'NONE') {
+            withCredentials([sshUserPrivateKey(credentialsId: sshCreds, keyFileVariable: 'ANSIBLE_SSH_KEY')]) {
+                withEnv(["ANSIBLE_HOST_KEY_CHECKING=False"]) {
+                    sh """
+                        cp "\$ANSIBLE_SSH_KEY" ansiblekey.pem
+                        chmod 400 ansiblekey.pem
+                        ansible-playbook -i ${inventory} ${playbook}
+                    """
                 }
-            } else {
-                sh "ansible-playbook -i ${inventory} ${playbook} ${becomeFlag} ${extraVars}"
             }
         }
-    }
-
-    if (sshCreds && sshCreds != 'NONE') {
-        withCredentials([sshUserPrivateKey(credentialsId: sshCreds, keyFileVariable: 'ANSIBLE_SSH_KEY')]) {
-            withEnv(["ANSIBLE_HOST_KEY_CHECKING=False"]) {
-                runPlaybook()
-            }
-        }
-    } else {
-        runPlaybook()
     }
 }
